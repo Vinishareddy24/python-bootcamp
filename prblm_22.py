c8436fdc1a41f74eec34dc43e9cd63907414dd9a
vinisha reddy <128719047+Vinishareddy24@users.noreply.github.com>
2024-07-22 21:16:06 +0530

    DAY_4
    DAY_4

---
+++ prblm_22.py
@@ -0,0 +1,13 @@
+#find LCM of 2 numbers
+a=int(input("enter a number: "))
+b=int(input("enter a number: "))
+if a>b:
+    greater=a
+else:
+    greater=b
+while(True):
+    if(greater%a==0)and(greater%b==0):
+        LCM=greater
+        break
+    greater+=1
+print("LCM:",LCM)
