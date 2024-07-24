22c85047969ebf820ca0387e3e5106931aa8554a
vinisha reddy <128719047+Vinishareddy24@users.noreply.github.com>
2024-07-24 20:59:44 +0530

    Day_6
    Day_6

---
+++ oops.py
@@ -0,0 +1,30 @@
+#creating class
+class Myclass:
+    def add(a,b):
+        return a+b
+#creating objects
+obj1=Myclass
+print(obj1.add(2,5))
+print(obj1.add(12,5))
+
+
+# creating add,sub,mul,div,mod class:
+class Myclass:
+    def add(a,b):
+        return a+b
+    def sub(a,b):
+        return a-b
+    def mul(a,b):
+        return a*b
+    def mod(a,b):
+        return a%b
+    def div(a,b):
+        return a/b
+obj1=Myclass
+obj2=Myclass
+print(obj1.add(2,5))
+print(obj2.sub(12,5))
+
+
+
+
